ed2ca2ed95fec1d339f0cfc4051699a1352cef9b
JongKook <whdrnrdl789@naver.com>
2024-01-15 23:48:31 +0900

    [feat]: Simple Chat Test

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// io.on('connection', (socket) => {
+//   // console.log('a user connected');
+//   socket.on("disconnect", () => console.log("user disconnect"));
+//   socket.on("chat message", (msg) => {
+//     console.log('message: ' + msg);
+//   })
+// });
+
+io.on("connection", (socket) =>{
+  //  socket.broadcast.emit("hi")
+   socket.on("chat message", (msg) =>{
+    io.emit("chat message", msg);
+   });
+
+   socket.on("disconnect", () => console.log("Disconnected"))
+  });
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
